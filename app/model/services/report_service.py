7ba8fcebab3e7cd72410f952413161723e3a9a51
FrederikBA <frederikbandersen98@gmail.com>
2022-05-02 15:21:21 +0200

    pandas dataframe from papp data

--- app/model/services/report_service.py
+++ app/model/services/report_service.py
@@ -1,12 +1,5 @@
 import pandas as pd
-import numpy as np
 
-data = np.array([['','Col1','Col2','col3'],
-                ['Row1',1,2,3],
-                ['Row2',4,5,6],
-                ['Row3',7,8,9]])
+df = pd.read_csv('../../data/papp_kgslyngby.csv', sep=";")
 
-pd.DataFrame(data)
-df = pd.DataFrame(data=data[1:4,1:4], columns=data[0,1:4], index=data[1:4,0])
-
-print(df.head())
+print(df)
