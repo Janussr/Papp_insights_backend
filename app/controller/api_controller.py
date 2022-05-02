def0ea0b2f33fa420c09b4da838886c1a6cf1161
FrederikBA <frederikbandersen98@gmail.com>
2022-05-02 10:32:05 +0200

    main > apicontroller

--- app/controller/api_controller.py
+++ app/controller/api_controller.py
@@ -6,3 +6,8 @@ app = FastAPI(title="FastAPI, Docker, and Traefik")
 @app.get("/")
 def read_root():
     return {"hello": "world"}
+
+
+@app.get("/report")
+def get_report():
+    return {"report": "belægningsgrad"}
