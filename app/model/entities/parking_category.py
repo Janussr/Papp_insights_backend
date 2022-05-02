84c39b33ff2367c8359ebed3bc83284c640e982e
FrederikBA <frederikbandersen98@gmail.com>
2022-05-02 14:57:41 +0200

    refactor models

--- app/model/entities/parking_category.py
+++
@@ -1,5 +0,0 @@
-class ParkingCategory():
-    def __init__(self, category_name, category_description):
-        self.category_name = category_name
-        self.category_description = category_description
-
---
+++ app/model/models.py
@@ -0,0 +1,30 @@
+class Report():
+
+    def __init__(self, report_name, parking_areas, parking_categories, time_filter):
+        self.report_name = report_name
+        #self.time_filter = TimeFilter(xxx,xxx,xxx)
+        self.parking_areas = []         
+        self.parking_categories = []        
+        for parking_category in parking_categories:
+            new_category = ParkingCategory(
+                parking_category.category_name,
+                parking_category.category_description,
+            )
+            self.parking_categories.append(new_category)
+
+class ParkingCategory():
+    def __init__(self, category_name, category_description):
+        self.category_name = category_name
+        self.category_description = category_description
+
+class ParkingOverview():
+
+    def __init__(self, booth_type, parking_categories):
+        self.booth_type = booth_type
+        self.parking_categories = []
+        for parking_category in parking_categories:
+            new_category = ParkingCategory(
+                parking_category.category_name,
+                parking_category.category_description,
+            )
+            self.parking_categories.append(new_category)
