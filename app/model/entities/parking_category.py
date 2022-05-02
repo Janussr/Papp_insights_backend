590777560124eb6a692519333792eb8d5d0afce6
Janussr <Janusstivang@gmail.com>
2022-05-02 11:46:47 +0200

    parking_category

---
+++ app/model/entities/parking_category.py
@@ -0,0 +1,5 @@
+class ParkingCategory():
+    def __init__(self, category_name, category_description):
+        self.category_name = category_name
+        self.category_description = category_description
+
