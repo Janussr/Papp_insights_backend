e95fe9c567133b738823234f108e1491ef9e5862
RasmusDalgaard <cph-rd92@cphbusiness.dk>
2022-05-05 15:07:42 +0200

    comma sep

--- app/tester.py
+++ app/tester.py
@@ -4,7 +4,7 @@ import pandas as pd
 def get_report(id):
     reports_df = pd.read_csv('data/reports.csv', sep=',')
     reports_df.set_index('id')
-    print(f'{id}')
+    print(reports_df)
     report = reports_df[reports_df['id'] == f'{id}']
     print(report)
 
