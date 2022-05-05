4b729f3a63d2f80f2dc7a4accccebe90eefcc269
FrederikBA <frederikbandersen98@gmail.com>
2022-05-05 15:08:25 +0200

    stuff

--- app/tester.py
+++ app/tester.py
@@ -2,10 +2,8 @@ from os import sep
 import pandas as pd
 
 def get_report(id):
-    reports_df = pd.read_csv('data/reports.csv', sep=',')
-    reports_df.set_index('id')
-    print(f'{id}')
-    report = reports_df[reports_df['id'] == f'{id}']
+    df = pd.read_csv('data/reports.csv', sep=',')
+    report = df['id'].str.contains(id) == True
     print(report)
 
 get_report(1)
