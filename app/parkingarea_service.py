71b2c783866274950fd392fc9eb9f1239f52251d
RasmusDalgaard <cph-rd92@cphbusiness.dk>
2022-05-18 14:04:22 +0200

    module importing fix

--- app/parkingarea_service.py
+++ app/parkingarea_service.py
@@ -1,6 +1,14 @@
 import pandas as pd
+import os
+import sys
 
-df = pd.read_csv('app/data/papp_kgslyngby.csv', sep=';')
+current = os.path.dirname(os.path.realpath(__file__))
+# Getting the parent directory name where the current directory is present.
+parent = os.path.dirname(current)
+# adding the parent directory to the sys.path.
+sys.path.append(parent)
+
+df = pd.read_csv('data/papp_kgslyngby.csv', sep=';')
 
 def all_parking_areas():
     unique_garageCodes = df['garageCode'].unique()
